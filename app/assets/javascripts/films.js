a84b77056759bd2f2e3f1874c2ec366f3aeb15b1
Scott O'Dea <odeascott@gmail.com>
2019-06-08 14:57:43 +1000

    rows move on page load depending upon local storage

--- app/assets/javascripts/films.js
+++ app/assets/javascripts/films.js
@@ -1,16 +1,31 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 window.onload = function() {
+  initialLoadFav();
   handleFavouriteClick();
 };
+
+function initialLoadFav() {
+  let favourite_films = getStorage();
+  const table_rows = document.querySelector("table").rows;
+  for (let i = 0; i < table_rows.length; i++) {
+    let title = formatCell(table_rows[i].cells[0].outerHTML);
+    if (favourite_films.includes(title)) {
+      let button = table_rows[i].cells[3].childNodes[0];
+      preference = "Favourite";
+      button.innerText = "Unfavourite";
+      button.style.backgroundColor = "#b92f2f";
+      moveRow(table_rows[i], preference);
+    }
+  }
+}
 
 const handleFavouriteClick = () => {
   document.addEventListener("click", function(e) {
     const { className, innerHTML, style } = e.target;
     const red = "#b92f2f";
     const green = "#4caf50";
-    let favourite_films =
-      JSON.parse(localStorage.getItem("favourite_films")) || [];
+    let favourite_films = getStorage();
     if (className === "favourite") {
       let row = e.path[2];
       let title = formatCell(row.cells[0].outerHTML);
@@ -18,10 +33,10 @@ const handleFavouriteClick = () => {
       if (innerHTML === "Favourite") {
         style.backgroundColor = red;
         e.target.innerHTML = "Unfavourite";
-        return favourite(row, favourite_films, title, preference);
+      } else {
+        style.backgroundColor = green;
+        e.target.innerHTML = "Favourite";
       }
-      style.backgroundColor = green;
-      e.target.innerHTML = "Favourite";
       favourite(row, favourite_films, title, preference);
     }
   });
@@ -56,4 +71,8 @@ function moveRow(row, preference) {
 //Regex to remove HTML tags from cell content
 function formatCell(cell) {
   return cell.replace(/<[^>]*>/g, "");
+}
+
+function getStorage() {
+  return JSON.parse(localStorage.getItem("favourite_films")) || [];
 }
