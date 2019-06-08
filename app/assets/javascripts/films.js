ccc6b3badc1894f8c58ef2732f965c40631ddb73
Scott O'Dea <odeascott@gmail.com>
2019-06-08 12:52:56 +1000

    added local storage functionality, refactored Javascript code

--- app/assets/javascripts/films.js
+++ app/assets/javascripts/films.js
@@ -6,29 +6,54 @@ window.onload = function() {
 
 const handleFavouriteClick = () => {
   document.addEventListener("click", function(e) {
-    const { className, innerText } = e.target;
+    const { className, innerHTML, style } = e.target;
     const red = "#b92f2f";
     const green = "#4caf50";
-    if (className === "favourite" && innerText === "Favourite") {
-      e.target.style.backgroundColor = red;
-      e.target.innerHTML = "Unfavourite";
-      moveToTop(e.path[2]);
-    } else if (className === "favourite") {
-      e.target.style.backgroundColor = green;
+    let favourite_films =
+      JSON.parse(localStorage.getItem("favourite_films")) || [];
+    if (className === "favourite") {
+      let row = e.path[2];
+      let title = formatCell(row.cells[0].outerHTML);
+      let preference = innerHTML;
+      if (innerHTML === "Favourite") {
+        style.backgroundColor = red;
+        e.target.innerHTML = "Unfavourite";
+        return favourite(row, favourite_films, title, preference);
+      }
+      style.backgroundColor = green;
       e.target.innerHTML = "Favourite";
-      moveToBottom(e.path[2]);
+      favourite(row, favourite_films, title, preference);
     }
   });
 };
 
-function moveToTop(row) {
-  let table_body = document.querySelector("table");
-  row.parentNode.removeChild(row);
-  table.insertBefore(row, table.childNodes[1]);
+//handle row movement and local storage
+function favourite(row, favourite_films, title, preference) {
+  moveRow(row, preference);
+  if (preference === "Favourite") {
+    favourite_films.push(title);
+    return localStorage.setItem(
+      "favourite_films",
+      JSON.stringify(favourite_films)
+    );
+  }
+  let index = favourite_films.indexOf(title);
+  favourite_films.splice(index, 1);
+  localStorage.setItem("favourite_films", JSON.stringify(favourite_films));
 }
 
-function moveToBottom(row) {
+//If row is favourited move to top of list, when unfavourited move to bottom
+function moveRow(row, preference) {
   let table_body = document.querySelector("table");
+  if (preference === "Favourite") {
+    row.parentNode.removeChild(row);
+    return table.insertBefore(row, table.childNodes[1]);
+  }
   row.parentNode.removeChild(row);
   table.appendChild(row);
+}
+
+//Regex to remove HTML tags from cell content
+function formatCell(cell) {
+  return cell.replace(/<[^>]*>/g, "");
 }
